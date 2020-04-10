ee13309502bfaf39c97afd37371ca71a44eadc00
Ramya Erramilli <e.ramya.103@gmail.com>
2020-04-10 07:34:25 +0530

    Commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,20 +1,82 @@
 //
 //  ViewController.swift
 //  EggTimer
-//
-//  Created by Ramya Seshagiri on 10/04/20.
-//  Copyright © 2020 Ramya Seshagiri. All rights reserved.
-//
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let eggTimes = ["Soft":3, "Medium":4,"Hard":7]
+
+    var secondsRemaining = 60
+    var secondsPassed = 0
+    var totalTime = 0
+    var player = AVAudioPlayer()
+    var timer = Timer()
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var progressBarOutlet: UIProgressView!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+//        print(sender.currentTitle)
+        
+//        if hardness=="Soft"{
+//            print(softTime)
+//        }else if hardness=="Medium"{
+//            print(mediumTime)
+//        }else{
+//            print(hardTime)
+//        }
+//        switch hardness {
+//        case "Soft":
+//            print(softTime)
+//        case "Medium":
+//            print(mediumTime)
+//        case "Hard":
+//            print(hardTime)
+//        default:
+//            print("Error")
+//        }
+        
+        timer.invalidate()
+        progressBarOutlet.progress = 0.0
+        secondsPassed = 0
+        labelOutlet.text = hardness!
+        var time = eggTimes[hardness!]
+        secondsRemaining = time!
+        totalTime = time!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        timer.fire()
     }
+    
+    @objc func updateTimer(){
+        if secondsPassed<totalTime{
 
+            secondsPassed += 1
+//            secondsRemaining -= 1
+            var percentageProgress = Float(secondsPassed)/Float(totalTime)
+            progressBarOutlet.progress = Float(percentageProgress)
+        }
+        else{
+            timer.invalidate()
+            labelOutlet.text = "DONE!"
+            playAlarm()
 
+        }
+    }
+    
+    func playAlarm(){
+        do{
+        var url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try AVAudioPlayer(contentsOf: url!)
+        player.play()
+        }
+        catch let error{
+            print("Error")
+        }
+        
+    }
+    
 }
-
